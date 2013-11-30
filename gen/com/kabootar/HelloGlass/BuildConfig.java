481c5c8351ef9c70dfcd421589feb045b6f0b6a5
Ankit Shankar <ankitshankar@gmail.com>
2013-11-30 14:14:07 -0800

    Remove generated files

--- gen/com/kabootar/HelloGlass/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.kabootar.HelloGlass;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
